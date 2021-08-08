a619950d52aed2a4a07e131b5e0b933f0b695237
fralabi <66752210+fralabi@users.noreply.github.com>
2021-08-08 15:20:03 +0200

    Update Binary Search.cpp

--- Algorithms/Binary Search.cpp
+++ Algorithms/Binary Search.cpp
@@ -49,23 +49,22 @@ int main(void)
 #include <bits/stdc++.h>// std::cout
 #include <algorithm>    // std::binary_search, std::sort
 #include <vector>       // std::vector
+using namespace std;
 
 
 int main () {
-  int myints[] = {1,2,3,4,5,4,3,2,1};
-  std::vector<int> v(myints,myints+9);                         // 1 2 3 4 5 4 3 2 1
+	int myints[] = {1,2,3,4,5,4,3,2,1};
+	vector<int> v(myints,myints+9);                         
 
-  // using default comparison:
-  std::sort (v.begin(), v.end());
+	// using default comparison:
+	sort (v.begin(), v.end());
 
-  std::cout << "looking for a 3... ";
-  if (std::binary_search (v.begin(), v.end(), 3))
-    std::cout << "found!\n"; else std::cout << "not found.\n";
+	cout << "looking for a 3... ";
+	if (binary_search (v.begin(), v.end(), 3))
+		cout << "found!\n"; 
+	else 
+		cout << "not found.\n";
 
-  // using myfunction as comp:
-  std::sort (v.begin(), v.end(), myfunction);
-
-  std::cout << "looking for a 6... ";
-  
-  return 0;
+	// using myfunction as comp:
+	return 0;
 }
